2ead22773161f84590e1a49a238741cca9b59c83
Plxyz08 <sebastianmunoz603@gmail.com>
2025-02-15 11:55:17 -0500

    solucion cors

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ class Server {
         this.app.use(express.json());
         this.app.use(bodyParser.json());
         this.app.use(cors({
-            origin: ['http://localhost:3000', 'https://lalista.onrender.com'],
+            origin: ['http://localhost:3000', 'https://lalista.onrender.com', 'https://lalista.vercel.app'],
             methods: ['GET', 'POST', 'PUT', 'DELETE'],
             allowedHeaders: ['Content-Type', 'Authorization', 'x-token']
         }));
